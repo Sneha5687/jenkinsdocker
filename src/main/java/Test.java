64a3c06293788e386057319af4356a2148fbc784
lipelandim <felandim@gmail.com>
2021-12-11 19:04:54 -0300

    adicionado

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,6 +1,6 @@
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("Hello for Jenkins");
+        System.out.println("Hello for Jenkins test");
     }
 }
